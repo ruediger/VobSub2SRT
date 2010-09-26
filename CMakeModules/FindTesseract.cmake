4b22e0a01323461541b3a767d51215f557a2688c
Rüdiger Sonderfeld <ruediger@c-plusplus.de>
2010-09-26 17:45:04 +0200

    Tesseract: search and link libtiff

--- CMakeModules/FindTesseract.cmake
+++ CMakeModules/FindTesseract.cmake
@@ -14,6 +14,13 @@ find_library(Tesseract_LIBRARIES NAMES tesseract_full
   /usr
   /usr/local)
 
+find_library(Tiff_LIBRARY NAMES tiff
+  HINTS
+  /usr
+  /usr/local)
+
+set(Tesseract_LIBRARIES ${Tesseract_LIBRARIES} ${Tiff_LIBRARY})
+
 include(FindPackageHandleStandardArgs)
 
 find_package_handle_standard_args(Tesseract DEFAULT_MSG Tesseract_LIBRARIES Tesseract_INCLUDE_DIR)
